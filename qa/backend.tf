be67a3d0d3a3f3ce5625c8aed21ffa3d61a8e793
visakh <visakh.us@epiuse.com>
2021-10-19 15:36:20 +0530

    qa

--- qa/backend.tf
+++ qa/backend.tf
@@ -1,9 +1,8 @@
 terraform {
-  required_version = "~> 0.14.8"
   backend "s3" {
     shared_credentials_file = "~/.aws/credentials"
     bucket                  = "visakhus"
-    key                     = "test/terraform-state/terraform.tfstate"
+    key                     = "testqa/terraform-state/terraform.tfstate"
     region                  = "us-west-2"
   }
 
