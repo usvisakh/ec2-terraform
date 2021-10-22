676808efadda62676132f419e786afdf1c39db44
visakh <visakh.us@epiuse.com>
2021-10-22 23:36:55 +0530

    script

---
+++ dev/create_user.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+
+user_var=$1
+pub_key=$2
+msg=`echo  "{\n    user    = "$user_var"\n    pub_key = ["$pub_key"]\n  },"`
+number=`grep -wn sftp_users_with_keys sample.txt | cut -d: -f1`
+number1=$(($number+1))
+echo $number1
+echo "$number"
+echo "$msg"
+sed -i "$number1"i" $msg" terraform.tfvars
